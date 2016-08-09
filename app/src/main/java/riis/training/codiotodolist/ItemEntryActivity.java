0c3e37c99f9a537e9dd533484435605aaa25f4d5
John <jdefour@riis.com>
2016-08-09 16:22:37 -0400

    added second screen with material design components for some practice

--- app/src/main/java/riis/training/codiotodolist/ItemEntryActivity.java
+++ app/src/main/java/riis/training/codiotodolist/ItemEntryActivity.java
@@ -2,12 +2,20 @@ package riis.training.codiotodolist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class ItemEntryActivity extends AppCompatActivity {
+
+    EditText nameText, descriptionText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_entry);
+
+        nameText = (EditText) findViewById(R.id.nameField);
+        descriptionText = (EditText) findViewById(R.id.descriptionField);
+
+
     }
 }
--- app/src/main/java/riis/training/codiotodolist/MainActivity.java
+++ app/src/main/java/riis/training/codiotodolist/MainActivity.java
@@ -1,9 +1,13 @@
 package riis.training.codiotodolist;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
@@ -16,11 +20,15 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView.LayoutManager layoutManager;
     ArrayList<Item> items;
     ItemAdapter adapter;
+    FloatingActionButton fab;
+    Activity thisActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        thisActivity = this;
 
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         items = new ArrayList<>();
@@ -38,5 +46,14 @@ public class MainActivity extends AppCompatActivity {
 
         adapter = new ItemAdapter(items);
         recyclerView.setAdapter(adapter);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(thisActivity, ItemEntryActivity.class);
+                startActivityForResult(intent, 1);
+            }
+        });
     }
 }
