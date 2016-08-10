07ae9cb3938b2218bef40467f96198eebe186ecb
John <jdefour@riis.com>
2016-08-10 10:52:44 -0400

    Added saving functionality and more material design elements

--- app/src/main/java/riis/training/codiotodolist/ItemEntryActivity.java
+++ app/src/main/java/riis/training/codiotodolist/ItemEntryActivity.java
@@ -3,15 +3,18 @@ package riis.training.codiotodolist;
 import android.app.Activity;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 
 public class ItemEntryActivity extends AppCompatActivity {
 
     EditText nameText, descriptionText;
     FloatingActionButton fabSave;
+    RelativeLayout rootLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +25,25 @@ public class ItemEntryActivity extends AppCompatActivity {
         descriptionText = (EditText) findViewById(R.id.descriptionField);
 
         fabSave = (FloatingActionButton) findViewById(R.id.fabSave);
+
+        rootLayout = (RelativeLayout) findViewById(R.id.itemEntryLayout);
+
         fabSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent returnIntent = new Intent();
-                returnIntent.putExtra("name", nameText.getText().toString());
-                returnIntent.putExtra("description", descriptionText.getText().toString());
-                setResult(Activity.RESULT_OK, returnIntent);
-                finish();
+                String name = nameText.getText().toString();
+                String description = descriptionText.getText().toString();
+                if (!name.isEmpty()) {
+
+
+                    Intent returnIntent = new Intent();
+                    returnIntent.putExtra("name", name);
+                    returnIntent.putExtra("description", description);
+                    setResult(Activity.RESULT_OK, returnIntent);
+                    finish();
+                } else {
+                    Snackbar.make(rootLayout, "Please put in a name.", Snackbar.LENGTH_LONG).show();
+                }
             }
         });
 
